88da28d3a28e27eed1cc212f4030b56861de273a
Claire Nord <cnord@mit.edu>
2019-03-02 22:06:07 -0500

    Refactor plot

--- plot.py
+++ plot.py
@@ -1,7 +1,18 @@
+from font import Font
 from glyph import Glyph
 
-if __name__ == "__main__":
-    letter = "A"
-    font = "fonts/cambam5.ttx"
-    letter = Glyph.from_font(font, letter)
+FONT = "fonts/cambam5.ttx"
+
+
+def plot_letter(letter):
+    letter = Glyph.from_font(FONT, letter)
     letter.plot()
+
+
+def plot_font():
+    font = Font(FONT)
+    font.plot()
+
+
+if __name__ == "__main__":
+    plot_letter("Y")
